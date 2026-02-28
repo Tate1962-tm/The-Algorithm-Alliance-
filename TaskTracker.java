1e730e23ab33610d5b9fc97f8e1343f0ae11d035
Tate1962-tm <tatendamachirori62@gmail.com>
2026-02-28 03:02:49 -0600

    Create TaskTracker.java

---
+++ TaskTracker.java
@@ -0,0 +1,21 @@
+//MEMBER NAME      : TATENDA MACHIRORI
+//CLASS            : CS3354
+
+// TaskTracker.java
+
+import java.util.List;
+
+public class TaskTracker {
+    // Instance variables
+    private List<Task> tasks;
+
+    // Public interface
+    public TaskTracker();
+    public void addTask(String description);
+    public void markTaskCompleted(String description);
+    public List<String> getPendingTasks();
+    public void deleteTask(String description);
+    // Additional useful methods
+    public List<String> getCompletedTasks();
+    public int getTaskCount();
+}
