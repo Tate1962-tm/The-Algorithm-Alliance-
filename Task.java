9ffeecfe7d51849bf339826df4c3e853299d4957
Tate1962-tm <tatendamachirori62@gmail.com>
2026-02-28 02:59:14 -0600

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,16 @@
+//MEMBER NAME : TATENDA MACHIRORI
+//CLASS       : CS3354
+
+  // Task.java
+
+public class Task {
+    // Instance variables
+    private String description;
+    private boolean completed;
+
+    // Public interface
+    public Task(String description);
+    public String getDescription();
+    public boolean isCompleted();
+    public void markCompleted();
+}
