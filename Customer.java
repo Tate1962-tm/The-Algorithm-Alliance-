8163a2d3f36a9c0ccad19ec66950f72ff685b74b
Tate1962-tm <tatendamachirori62@gmail.com>
2026-03-08 19:35:56 -0500

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,15 @@
+//TATENDA MACHIRORI
+//CS3354
+package customer;
+
+public class Customer {
+    public String customerName;
+    public int customerId;
+    public String address;
+
+    public Customer(String name, int id, String address) {
+        this.customerName = name;
+        this.customerId = id;
+        this.address = address;
+    }
+}
